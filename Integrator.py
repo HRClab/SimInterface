23416f89c5a3e3912995a1b38dc1dd799a3b5ff3
Andy <andy.lamperski@gmail.com>
2015-08-14 10:32:16 -0500

    made a linear quadratic system class and made Integrator a subclass

--- Integrator.py
+++ Integrator.py
@@ -1,14 +1,16 @@
-import MarkovDecisionProcess as MDP
+import LinearQuadraticSystem as LQS
 
 
-class Integrator(MDP.MarkovDecisionProcess):
+class Integrator(LQS.LinearQuadraticSystem):
+    """
+    This is the simplest linear system dynamical system
+    """
     def __init__(self):
         self.dt = 0.1
         self.x0 = 1.0
-        
-    def costStep(self,x,u,k):
-        return self.dt*(x*x+u*u)
-
-    def step(self,x,u,k):
-        return x+self.dt*u
-        
+        A = 1.
+        B = self.dt
+        Q = self.dt * 1.
+        R = self.dt * 1.
+        LQS.LinearQuadraticSystem.__init__(self,A=A,B=B,Cxx=Q,Cuu=R)
+                
