b3365980ac6c7c2bef605f13d2e6173c6746a15b
Andy <andy.lamperski@gmail.com>
2015-10-09 18:49:15 -0500

    brought in basic files

---
+++ SimInterface/__init__.py
@@ -0,0 +1,41 @@
+# Import basic system types. 
+
+from MarkovDecisionProcess import MarkovDecisionProcess
+from MarkovDecisionProcess import differentialEquation
+from MarkovDecisionProcess import driftDiffusion
+from MarkovDecisionProcess import deterministicSubsystem
+
+# Import Input augmenter
+from MarkovDecisionProcess import augmentInput
+
+# Import Linear Quadratic Systems
+from linearQuadraticSystem import linearQuadraticSystem
+from linearQuadraticSystem import linearQuadraticStochasticSystem
+
+# Import Linear Quadratic Helper Functions
+from linearQuadraticSystem import buildDynamicsMatrix
+from linearQuadraticSystem import buildCostMatrix
+
+# Import Lagrangian Systems
+from LagrangianSystem import LagrangianSystem
+from LagrangianSystem import inputAugmentedLagrangian
+
+# Import Newton Euler System
+from NewtonEulerSystem import NewtonEulerSys
+
+from Controller import openLoopPolicy
+from Controller import flatOpenLoopPolicy
+from Controller import staticGain
+from Controller import varyingAffine
+from Controller import flatVaryingAffine
+from Controller import staticFunction
+
+from linearQuadraticControl import linearQuadraticRegulator
+from linearQuadraticControl import modelPredictiveControl
+from linearQuadraticControl import iterativeLQR
+from linearQuadraticControl import approximateLQR
+
+from samplingControl import samplingOpenLoop
+from samplingControl import samplingStochasticAffine
+from samplingControl import samplingMPC
+from samplingControl import gibbsOpenLoop
