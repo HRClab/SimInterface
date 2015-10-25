df91b56ee50727d98b07e8639ba1cb8a3ac3e559
Andy <andy.lamperski@gmail.com>
2015-10-25 18:08:41 -0500

    got simplest adaptive control working

--- SimInterface/adaptiveControl.py
+++ SimInterface/adaptiveControl.py
@@ -35,14 +35,28 @@ def unstackSymmetricMatrix(v):
 
     M = UpperTriangle+UpperTriangle.T+np.diag(Diagonal)
     return M
+
+def explorationGain(stateGain,Covariance,Horizon):
+    CovChol = la.cholesky(Covariance,lower=True)
+    p,n = stateGain.shape
+    W = rnd.randn(Horizon,n)
+
+    Gain = np.zeros((Horizon,p,1+n))
+    for k in range(Horizon):
+        Gain[k,0,:] = np.dot(CovChol,W[k])
+        Gain[k,1:,:] = stateGain
+        
+    return Gain
+class actorCriticLQR(ctrl.staticGain):
+    """
+    Natual Actor Critic method applied to linear quadratic systems. 
     
-class actorCriticLQR(ctrl.varyingAffine):
+    In particular, This is a variant of Table 1 of 
+    "Natural Actor-Critic" by Peters, Vijayakumar, and Schaal
     """
-    Natual Actor Critic method applied to linear quadratic systems
-    """
-    def __init__(self,SYS,Horizon=1,
-                 EpisodeLength=1,EpisodeCount=1,
-                 TraceDecayRate=0.,DiscountRate=1.,
+    def __init__(self,SYS,
+                 EpisodeLength=1,EpisodeCount=1,stepSizeConstant=1.,
+                 TraceDecayFactor=0.,DiscountFactor=1.,ForgettingFactor=1,
                  stateGain=None,Covariance=None,
                  *args,**kwargs):
         p = SYS.NumInputs
@@ -58,30 +72,87 @@ class actorCriticLQR(ctrl.varyingAffine):
             Covariance = np.eye(n)
 
 
-        AffineGain = np.zeros((EpisodeLength,p,n+1))
-        ctrl.varyingAffine.__init__(self,AffineGain,Horizon=EpisodeLength,
-                                    *args,**kwargs)
 
-        
-        self.stateGain = stateGain
-        self.Covariance = Covariance
-        self.resetGains()
+        numFeatures = n*(n+1) + n*p + 1 
 
+        # Auxilliary temporal difference variables
+        z = np.zeros(numFeatures)
+        A = np.zeros((numFeatures,numFeatures))
+        b = np.zeros(numFeatures)
+
+        x0 = SYS.x0
+        #### Actor Critic Learning ####
         for episode in range(EpisodeCount):
-            X,U,Cost = SYS.simulatePolicy(self)
+            AffineGain = explorationGain(stateGain,Covariance,EpisodeLength)
+            learningController = ctrl.varyingAffine(AffineGain,
+                                                    Horizon=EpisodeLength)
 
-            
+            if episode > 0:
+                SYS.x0 = X[-1]
+            X,U,Cost = SYS.simulatePolicy(learningController)
+            if episode > 0:
+                SYS.x0 = x0
+                
+            ### Policy Evaluation by least squares temporal differences ###
 
-            
-        AffineGain = np.zeros((Horizon,p,n+1))
-        self.Gain = AffineGain
-        self.Horizon = Horizon
-        self.resetGains()
+            TrueCost = 0.
+            CostFactor = 1.
+            for k in range(EpisodeLength-1):
+                TrueCost += CostFactor * Cost[k]
+                CostFactor *= DiscountFactor 
+
+                xSquare = np.outer(X[k],X[k])
+                xSquareStacked = stackSymmetricMatrix(xSquare)
+
+                inputResidual = U[k] - np.dot(stateGain,X[k])
+                gainSolve = la.solve(Covariance,inputResidual,sym_pos=True)
+                gainDerivative = np.outer(gainSolve,X[k])
+                gainDerivativeStacked = np.reshape(gainDerivative,n*p)
+
+                invCov = la.inv(Covariance)
+                covDerivative = .5*(np.outer(gainSolve,gainSolve)-invCov)
+                covDerivativeStacked = stackSymmetricMatrix(covDerivative)
+
+                PhiHat = np.hstack((1,xSquareStacked,
+                                    gainDerivativeStacked,
+                                    covDerivativeStacked))
+
+                PhiTilde = np.zeros(numFeatures)
+                PhiTilde[0] = 1.
+                nextXSquare = np.outer(X[k+1],X[k+1])
+                nextXSquareStacked = stackSymmetricMatrix(nextXSquare)
+                PhiTilde[1:1+len(xSquareStacked)] = nextXSquareStacked
+                
+                z = TraceDecayFactor * z + PhiHat
+                A += np.outer(z,PhiHat-DiscountFactor*PhiTilde)
+                b += z*Cost[k]
+
+            # Optimal Feature Weights
+            featureWeights = la.solve(A,b)
+
+            constCost = featureWeights[0]
+            costMatrixStacked = featureWeights[1:n*(n+1)/2+1]
+            P = unstackSymmetricMatrix(costMatrixStacked)
+            costEst = np.dot(X[0],np.dot(P,X[0])) + constCost
+            print 'Episode Cost: %g, Estimated Episode Cost: %g' % (TrueCost,
+                                                                    costEst)
+                                                                    
+
+            policyGradient = featureWeights[n*(n+1)/2 + 1:]
+
+            Kdiff = np.reshape(policyGradient[:n*p],(p,n))
+            CovDiff = unstackSymmetricMatrix(policyGradient[n*p:])
+            stepSize = stepSizeConstant / (episode+1)
+
+            stateGain -= stepSize * Kdiff
+            Covariance -= stepSize * CovDiff
+
+            # Forget previous influence slightly
+            z = ForgettingFactor * z
+            A = ForgettingFactor * A
+            b = ForgettingFactor * b
+
+            # May need to figure out how to get the appropriate gain shape
+            ctrl.staticGain.__init__(self,gain=stateGain.squeeze(),NumInputs=p,
+                                     *args,**kwargs)
         
-    def resetGains(self):
-        CovChol = la.cholesky(self.Covariance,lower=True)
-        W = rnd.randn(self.Horizon,self.n)
-
-        for k in range(self.Horizon):
-            self.Gain[k,0,:] = np.dot(CovChol,W[k])
-            self.Gain[k,1:,:] = self.stateGain
