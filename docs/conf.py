fce9ba806aa3dea4544c1d8779ce3951e963ac4e
Andy <andy.lamperski@gmail.com>
2015-11-25 00:43:16 -0600

    mocked openblas

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@ import shlex
 
 import mock
  
-MOCK_MODULES = ['numpy', 'scipy', 'sympy', 'scipy.linalg']
+MOCK_MODULES = ['numpy', 'scipy', 'sympy', 'scipy.linalg','openblas']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
 
