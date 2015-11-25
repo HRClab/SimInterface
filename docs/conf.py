6736ada4ca64b7d674c11e29532b7fd5cefaea8c
Andy <andy.lamperski@gmail.com>
2015-11-25 00:16:29 -0600

    removed mocks

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ class Mock(MagicMock):
             return Mock()
 
 MOCK_MODULES = ['numpy', 'scipy', 'sympy']
-sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+# sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
