c70780802229e2301f6593eb5aa897cb02e0f547
Andy <andy.lamperski@gmail.com>
2015-12-07 11:02:08 -0600

    got parameters working

--- SimInterface/Variable.py
+++ SimInterface/Variable.py
@@ -7,39 +7,21 @@ Variable
 import numpy as np
 import pandas as pd
 
-class Parameter:
-    def __init__(self,label='Var',data=None,shape=None):
-        pass
-class Signal:
-    """
-    This is a variable
-
-    Parameters
-       label : str
-          what to call the variable
-
-       value : scalar or array_like
-
-    """
-
-    def __init__(self,label='Var',data=None,shape=None):
-        """
-
-        """
-
-        self.__createDataFrame(label,data,shape)
+class Variable:
+    def __init__(self,label='Var',data=None,shape=None,TimeStamps=None):
+        self.__createDataFrame(label,data,shape,TimeStamps)
         
         self.label = label
         self.Source = None
         self.Targets = []
 
-    def __createDataFrame(self,label,data,shape):
+    def __createDataFrame(self,label,data,shape,TimeStamps):
         """
         An internal function to create a pandas DataFrame object 
         """
         if shape is None:
             # Scalar
-            columns = label
+            columns = [label]
         else:
             NumEl = np.prod(shape)
             indices = range(NumEl)
@@ -53,6 +35,9 @@ class Signal:
                 dataMat = 0.0
             else:
                 dataMat = np.zeros((1,NumEl))
+        elif not isinstance(data,np.ndarray):
+            # Scalar input
+            dataMat = np.array([[data]])
         elif data.shape == shape:
             dataMat = np.array([data.flatten()])
         elif data.shape == (NumEl,):
@@ -66,11 +51,39 @@ class Signal:
                 dataMat = np.reshape(data,(len(data),NumEl))
                 
 
-        self.data = pd.DataFrame(dataMat,columns=columns)
+        T,n = dataMat.shape
+        if TimeStamps is None:
+            TimeStamps = np.arange(T)
+
+        self.data = pd.DataFrame(dataMat,
+                                 columns=columns,
+                                 index=TimeStamps[:T])
         
 
     def __getitem__(self,item):
         return self.data[item]
 
+class Parameter(Variable):
+    def __init__(self,label='Var',data=None,shape=None):
+        Variable.__init__(self,label,data,shape)
+    
+class Signal(Variable):
+    """
+    This is a variable
+
+    Parameters
+       label : str
+          what to call the variable
+
+       value : scalar or array_like
+
+    """
+
+    def __init__(self,label='Var',data=None,shape=None,TimeStamps=None):
+        """
+
+        """
+
+        Variable.__init__(self,label,data,shape,TimeStamps)
 
         
