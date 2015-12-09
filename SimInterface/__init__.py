91bcfeb93fef05adf89fdb700c4614656981e20b
Andy <andy.lamperski@gmail.com>
2015-12-09 10:36:20 -0600

    removed references to old code

--- SimInterface/__init__.py
+++ SimInterface/__init__.py
@@ -1,4 +1,3 @@
-#
 from Variable import Signal
 from Variable import Parameter
 from System import StaticFunction
@@ -6,41 +5,3 @@ from System import DifferentialEquation
 from System import System
 from System import Connect
 
-# Import basic system types. 
-
-from MarkovDecisionProcess import MarkovDecisionProcess
-from MarkovDecisionProcess import differentialEquation
-from MarkovDecisionProcess import driftDiffusion
-from MarkovDecisionProcess import deterministicSubsystem
-from MarkovDecisionProcess import smoothDiffEq
-
-# Import Input augmenter
-from MarkovDecisionProcess import augmentInput
-
-# Import Linear Quadratic Systems
-from linearQuadraticSystem import linearQuadraticSystem
-from linearQuadraticSystem import linearQuadraticStochasticSystem
-
-# Import Linear Quadratic Helper Functions
-from linearQuadraticSystem import buildDynamicsMatrix
-from linearQuadraticSystem import buildCostMatrix
-
-# Import Lagrangian Systems
-from LagrangianSystem import LagrangianSystem
-from LagrangianSystem import inputAugmentedLagrangian
-
-# Import basic control stuff
-from Controller import openLoopPolicy
-from Controller import flatOpenLoopPolicy
-from Controller import staticGain
-from Controller import varyingAffine
-from Controller import flatVaryingAffine
-from Controller import staticFunction
-
-from linearQuadraticControl import linearQuadraticRegulator
-from linearQuadraticControl import modelPredictiveControl
-from linearQuadraticControl import iterativeLQR
-from linearQuadraticControl import approximateLQR
-
-from adaptiveControl import naturalActorCritic
-from adaptiveControl import actorCriticLQR
--- docs/index.rst
+++ docs/index.rst
@@ -8,6 +8,30 @@ SimInterface Documentation
 
 Oh hey! This is the documentation for the SimInterface, a Python package for simulation and control. The package, and its documentation, are very much "Work in Progress", so this will likely change drastically on a regular basis.
 
+What Does the  SimInterface Do?
+-------------------------------
+
+The SimInterface is meant to be a modular environment for constructing and maniupulating dynamical systems. In particular, the main task of the SimInterface is to handle bookkeeping tasks such as the value of internal signals, which variables get passed to which functions, and so on. 
+
+Features
+~~~~~~~~
+
+* Construction of interconnected system from subsystems
+
+* Generation of vector fields that can be passed to simulators
+
+* Organization of signals in Pandas DataFrames 
+
+What doesn't the SimInterface Do?
+---------------------------------
+
+The SimInterface is not intended to be a full simulation environment. This code is to help with constructing and manipulating the systems. To simulate a system, you can use separate, dedicated tools, such as scipy.itegrate.
+
+By keeping the focus of the
+
+Add-Ons
+-------
+  
 .. toctree::
    :maxdepth: 1
 
